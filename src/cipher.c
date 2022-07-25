b4f77bf77d0ed705e0bedf21774cd830513dc6c2
lucioana <nikita.suslov.03@mail.ru>
2022-07-25 21:47:13 +0300

    quest 2

--- src/cipher.c
+++ src/cipher.c
@@ -1,26 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int read_file(char *path);
+int read_file(char *path, char *output);
+int write_file(char *path, char *add, char *output);
 char *read();
 int strlen_(char *str);
 
 int main() {
-    char *input = NULL;
+    char *input = NULL, *path = NULL;
     input = read();
 
     while (!(input && strlen_(input) == 2 && input[0] == '-' && input[1] == '1')) {
         if (input && strlen_(input) == 1 && input[0] == '1') {
-            char *path = read();
+            if (path != NULL)
+                free(path);
+
+            path = read();
 
             if (path == NULL) {
                 printf("n/a\n");
             } else {
-                if (read_file(path)) {
+                char *output = malloc(1);
+
+                if (read_file(path, output)) {
                     printf("n/a\n");
+                } else {
+                    printf("%s\n", output);
                 }
 
-                free(path);
+                free(output);
+            }
+        } else if (input && strlen_(input) == 1 && input[0] == '2') {
+            char *add = read();
+
+            if (path == NULL || add == NULL) {
+                printf("n/a\n");
+            } else {
+                char *output = malloc(1);
+
+                if (write_file(path, add, output)) {
+                    printf("n/a\n");
+                } else {
+                    printf("%s\n", output);
+                }
+
+                free(output);
+                free(add);
             }
         } else {
             printf("n/a\n");
@@ -36,9 +61,8 @@ int main() {
         free(input);
 }
 
-int read_file(char *path) {
+int read_file(char *path, char *output) {
     FILE *file = fopen(path, "r");
-    char *output = NULL;
     int flag = 0, len = 0;
 
     if (!file) {
@@ -55,9 +79,40 @@ int read_file(char *path) {
 
         if (feof(file) == 0) {
             flag = 1;
-        } else {
-            printf("%s\n", output);
-            free(output);
+        }
+
+        fclose(file);
+    }
+
+    return flag;
+}
+
+int write_file(char *path, char *add, char *output) {
+    FILE *file = fopen(path, "r+");
+    int flag = 0, len = 0;
+
+    if (!file) {
+        flag = 1;
+    } else {
+        fseek(file, 0, SEEK_END);
+        fputs(add, file);
+    }
+
+    if (!file) {
+        flag = 1;
+    } else {
+        fseek(file, 0, SEEK_SET);
+        int input = fgetc(file);
+
+        while (input != EOF) {
+            output = realloc(output, len + 1);
+            output[len] = input;
+            len++;
+            input = fgetc(file);
+        }
+
+        if (feof(file) == 0) {
+            flag = 1;
         }
 
         fclose(file);
